e1b10a0805c5627e3c8e809bfb6d186f3946967b
shirus172 <jeremy@wearewright.com>
2022-10-07 16:01:16 -0600

    fixed json issue

--- app.py
+++ app.py
@@ -12,12 +12,15 @@ create_log()
 # Example request
 def listUsers():
     # set the URL for the api request
+    # if query paramters are needed add them here
     url = "/users"
     # pass any data that needs to be in the body of the request
+    # should be a dict will be converted to json for you
     data = {}
     # specify RESTful action get,patch,put,post,delete
     action = "get"
-    # send the request
+    # send the request the send_reequest() function is imported from ./lib/req.py
+    # It takes 3 arguments action(the RESTFUL API verb), the url(everything after 'https://api.zoom.us/v2/'), and data(a dict that acts as the body of the request)
     response = send_request(action, url, data)
     # prettyPrint response
     printJSON(response)
--- lib/req.py
+++ lib/req.py
@@ -15,20 +15,21 @@ BASE_URL = 'https://api.zoom.us/v2/'
 def send_request(action, url, data):
     t = token()
     FINAL_URL = BASE_URL+url
+    d = json.dumps(data)
     headers = {'authorization': 'Bearer %s' % t,
                'content-type': 'application/json'}
     logging.debug("'URL: {0}', 'headers: {1}', 'body: {2}'".format(
         FINAL_URL, headers, data))
     if action == "post":
-        r = requests.post(FINAL_URL, headers=headers, data=data)
+        r = requests.post(FINAL_URL, headers=headers, data=d)
     elif action == "patch":
-        r = requests.patch(FINAL_URL, headers=headers, data=data)
+        r = requests.patch(FINAL_URL, headers=headers, data=d)
     elif action == "put":
-        r = requests.put(FINAL_URL, headers=headers, data=data)
+        r = requests.put(FINAL_URL, headers=headers, data=d)
     elif action == "get":
-        r = requests.get(FINAL_URL, headers=headers, data=data)
+        r = requests.get(FINAL_URL, headers=headers, data=d)
     elif action == "delete":
-        r = requests.delete(FINAL_URL, headers=headers, data=data)
+        r = requests.delete(FINAL_URL, headers=headers, data=d)
     if str(r.status_code).startswith('2'):
         logging.info("'Status: {0}', 'RESPONSE: {1}'".format(
             r.status_code, r.content))
